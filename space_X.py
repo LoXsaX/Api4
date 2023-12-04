e3e52cd60b3cc1e6910ae5605ec5b3b28ce5231b
LoXsaX <119329239+LoXsaX@users.noreply.github.com>
2023-12-04 16:59:32 +0300

    Изменил регистр названия переменных

--- space_X.py
+++ space_X.py
@@ -3,24 +3,24 @@ import os
 from uploade_url import uploade_url
 
 
-def get_url_photo_SpaceX():
+def get_url_photo_spacex():
     url = "https://api.spacexdata.com/v5/launches/"
     response = requests.get(url)
-    for link_photo_SpaceX in response.json():
-        if link_photo_SpaceX["links"]["flickr"]["original"]:
-            url_photo = link_photo_SpaceX["links"]["flickr"]["original"]
+    for link_photo_spacex in response.json():
+        if link_photo_spacex["links"]["flickr"]["original"]:
+            url_photo = link_photo_spacex["links"]["flickr"]["original"]
     return url_photo
 
 
 def fetch_spacex_last_launch(url_photo, folder_names):
     for number, link in enumerate(url_photo):
-        filename = f'SpaceX{number}.jpg'
+        filename = f'spacex{number}.jpg'
         path = os.path.join(folder_names, filename)
         uploade_url(link, path)
 
 
 def main():
-    get_url_photo_SpaceX()
+    get_url_photo_spacex()
 
 
 if __name__ == '__main__':
