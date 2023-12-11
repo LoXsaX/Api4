30e7791ee36e4fafab1e4ef698b08871459b54e8
LoXsaX <119329239+LoXsaX@users.noreply.github.com>
2023-12-11 16:19:49 +0300

    Сделал код более универсальным

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from pathlib import Path
 import os
 from os import listdir
 import datetime
-from fetch_NASA_apod import fetch_NASA_apod
-from get_epic_photo import get_epic_photo
-from space_X import fetch_spacex_last_launch, request_api_SpaceX
 from time import sleep
 import telegram
 import random
@@ -21,20 +18,15 @@ def loading_auto(tg_token, chat_id):
         file_path = os.path.join(folder, file)
         with open(file_path, 'rb') as f:
             bot.send_document(chat_id, document=f)
-        sleep(10)
+        sleep(14400)
 
 
 def main():
     load_dotenv()
-    nasa_key = os.environ['NASA_KEY']
     tg_token = os.environ['TG_TOKEN']
     chat_id= os.environ['CHAT_ID']
     folder_names = 'images'
     Path(folder_names).mkdir(parents=True, exist_ok=True)
-    url_photo = request_api_SpaceX()
-    get_epic_photo(folder_names, nasa_key)
-    fetch_NASA_apod(folder_names, nasa_key)
-    fetch_spacex_last_launch(url_photo, folder_names)
     loading_auto(tg_token, chat_id)
 
 
