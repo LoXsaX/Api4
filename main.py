1b0d88ae9f3f34aa2718877fa15b1c0e85b0cbd8
LoXsaX <119329239+LoXsaX@users.noreply.github.com>
2023-12-11 16:15:36 +0300

    Добавил секрет и исправил имя функции

--- main.py
+++ main.py
@@ -28,15 +28,16 @@ def main():
     load_dotenv()
     nasa_key = os.environ['NASA_KEY']
     tg_token = os.environ['TG_TOKEN']
-    chat_id = os.environ['TG_CHANNLE_ID']
+    chat_id= os.environ['CHAT_ID']
     folder_names = 'images'
     Path(folder_names).mkdir(parents=True, exist_ok=True)
     url_photo = request_api_SpaceX()
     get_epic_photo(folder_names, nasa_key)
     fetch_NASA_apod(folder_names, nasa_key)
     fetch_spacex_last_launch(url_photo, folder_names)
-    loading_auto(tg_token)
+    loading_auto(tg_token, chat_id)
 
 
 if __name__ == '__main__':
     main()
+
