405af91f2172059967c6c09068bdad1cbe6a3258
LoXsaX <119329239+LoXsaX@users.noreply.github.com>
2024-01-10 16:44:04 +0300

    Починил cript

--- get_epic_photo.py
+++ get_epic_photo.py
@@ -4,6 +4,7 @@ from uploade_url import uploade_url
 import datetime
 from dotenv import load_dotenv
 
+
 def get_epic_photo(folder_names, api_key):
     url = "https://api.nasa.gov/EPIC/api/natural/image"
     params = {'api_key': api_key, 'count': 5}
@@ -17,9 +18,14 @@ def get_epic_photo(folder_names, api_key):
         epic_link = f'https://api.nasa.gov/EPIC/archive/natural//{epic_time}/png/{epic_name}.png'
         path = os.path.join(folder_names, f'{epic_name}.png')
         uploade_url(epic_link, path, params)
+
+
+def main():
     load_dotenv()
     api_key = os.environ['NASA_KEY']
-    folder_names = 'images'
+    folder_names = 'images' 
+    get_epic_photo(folder_names, api_key)
 
-if __name__ == '__main__':
+
+if __name__ == '__main__':  
     main()
