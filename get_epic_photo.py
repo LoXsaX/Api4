11a9dd24279cce36b526a0fea0112e54c1963704
LoXsaX <119329239+LoXsaX@users.noreply.github.com>
2024-01-24 16:25:16 +0300

    Поправил множественное число

--- get_epic_photo.py
+++ get_epic_photo.py
@@ -5,7 +5,7 @@ import datetime
 from dotenv import load_dotenv
 
 
-def get_epic_photo(folder_names, api_key):
+def get_epic_photos(folder_name, api_key):
     url = "https://api.nasa.gov/EPIC/api/natural/image"
     count = 5
     params = {'api_key': api_key, 'count': count}
@@ -17,15 +17,15 @@ def get_epic_photo(folder_names, api_key):
         epic_time = datetime.datetime.fromisoformat(epic_date).strftime(
             "%Y/%m/%d")
         epic_link = f'https://api.nasa.gov/EPIC/archive/natural//{epic_time}/png/{epic_name}.png'
-        path = os.path.join(folder_names, f'{epic_name}.png')
+        path = os.path.join(folder_name, f'{epic_name}.png')
         uploade_url(epic_link, path, params)
 
 
 def main():
     load_dotenv()
     api_key = os.environ['NASA_KEY']
-    folder_names = 'images' 
-    get_epic_photo(folder_names, api_key)
+    folder_name = 'images' 
+    get_epic_photos(folder_name, api_key)
 
 
 if __name__ == '__main__':  
