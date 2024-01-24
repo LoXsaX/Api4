908d2afaa33696e151ac320efcc358da70e5767a
LoXsaX <119329239+LoXsaX@users.noreply.github.com>
2024-01-24 16:38:48 +0300

    Проверил статус ответа

--- space_x.py
+++ space_x.py
@@ -6,6 +6,7 @@ from uploade_url import uploade_url
 def get_url_photo_spacex(folder_name):
     url = "https://api.spacexdata.com/v5/launches/"
     response = requests.get(url)
+    response.raise_for_status()
     for link_photo_spacex in response.json():
         if link_photo_spacex["links"]["flickr"]["original"]:
             photo_urls = link_photo_spacex["links"]["flickr"]["original"]
