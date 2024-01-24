ef398013d9f12f1306eb2f101a1ae16f2b82911c
LoXsaX <119329239+LoXsaX@users.noreply.github.com>
2024-01-24 16:26:52 +0300

    Поправил множественное число

--- space_x.py
+++ space_x.py
@@ -3,21 +3,21 @@ import os
 from uploade_url import uploade_url
 
 
-def get_url_photo_spacex(folder_names):
+def get_url_photo_spacex(folder_name):
     url = "https://api.spacexdata.com/v5/launches/"
     response = requests.get(url)
     for link_photo_spacex in response.json():
         if link_photo_spacex["links"]["flickr"]["original"]:
-            photos_url = link_photo_spacex["links"]["flickr"]["original"]
-    for number, link in enumerate(photos_url):
+            photo_urls = link_photo_spacex["links"]["flickr"]["original"]
+    for number, link in enumerate(photo_urls):
         filename = f'spacex{number}.jpg'
-        path = os.path.join(folder_names, filename)
+        path = os.path.join(folder_name, filename)
         uploade_url(link, path)
 
 
 def main():
-    folder_names = 'images'
-    get_url_photo_spacex(folder_names)
+    folder_name = 'images'
+    get_url_photo_spacex(folder_name)
 
 
 if __name__ == '__main__':
